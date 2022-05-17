b400b25fc0c2c9d9f56484858499ded25d0eeb35
itstheanurag <gauravanurag36@gmail.com>
2022-05-17 12:34:47 +0530

    Second Commit

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -4,7 +4,8 @@ const urlSchema = new mongoose.Schema({
     urlCode: {
         type: String,
         unique: true,
-        trim: true
+        trim: true,
+        lowercase : true
     },
     longUrl: {
         type: String,
