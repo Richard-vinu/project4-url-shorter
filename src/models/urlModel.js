2e48f4e2a292a679c8aeaf23bd84e7988f1121de
Richard-vinu <richardnova1999@gmail.com>
2022-05-17 12:23:49 +0530

    urlModel

---
+++ src/models/urlModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    urlCode: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+    longUrl: {
+        type: String,
+        required: 'Long Url is required',
+        trim: true
+    },
+    shortUrl: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('urlShortnerProject_url', urlSchema)
---
+++ src/route/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post("/url/shorten");
+
+router.get("/:urlCode" );
+
+
+
+module.exports = router;
