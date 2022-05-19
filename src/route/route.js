18f3b9a790e62e7bc6466021ed8fec5ebc149dc1
itstheanurag <gauravanurag36@gmail.com>
2022-05-19 12:29:01 +0530

    Code Done

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const { shortUrl, redirectToSource } = require('../controller/urlController');
 const router = express.Router();
 
 
 
-router.post("/url/shorten", shortUrl);
+router.post("/url/shorten", shortUrl );
 router.get("/:urlCode", redirectToSource);
 
 
